97e015041fdee91095d01463c11bcdb6694d8f64
JoshATM <joshua.downes.747@accesstomusic.ac.uk>
2024-01-25 15:09:06 +0000

    25/01/2024

--- src/Navigation/NavigationBar.jsx
+++ src/Navigation/NavigationBar.jsx
@@ -1,8 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import Button from "../components/Button";
 import SideBarButton from "../components/SideBarButton";
-import Popup from '../components/LoginPopup';
 import { useState } from "react";
 import Logo from '../assets/GibJohnTutoringLogo.png';
 import LoginPopup from '../components/LoginPopup';
@@ -44,17 +42,10 @@ const NavigationBar = () => {
     document.getElementById('sideBar').style.left = '-100%';
   }
   
-  const CloseButton = () => {
-  };
-  
   const x = 1;
   if (x==1) {
     return (
     <>
-      <Welcome style={{ marginTop: localStorage.getItem('welcome')}}>
-        Sign Up Today and get 10% OFF! Use code WELCOME to redeem!
-        <StyledCloseButton onClick={CloseButton}>X</StyledCloseButton>
-      </Welcome>
       <StyledDiv>
         <StyledLogo src={Logo} onClick={() => { navigate('/') }} />
       </StyledDiv>
@@ -76,30 +67,14 @@ const NavigationBar = () => {
         </StyledInnerSideBar>
       </StyledSideBar>
       <CookieConsent id="CookieConsent" >
-        Cookie Consent
-        <button onClick={AcceptCookieConsent}>Accept</button>
-        <button onClick={DeclineCookieConsent}>Decline</button>
+        This website uses Cookies to enhance the user experience.
+        <CookieConsentButton onClick={AcceptCookieConsent}>Accept</CookieConsentButton>
+        <CookieConsentButton onClick={DeclineCookieConsent}>Decline</CookieConsentButton>
         <StyledCloseButton onClick={CloseCookieButton}>X</StyledCloseButton>
       </CookieConsent>
     </>
     )
   }
-
-  return (
-    <>
-      <Welcome style={{ marginTop: localStorage.getItem('welcome')}}>
-        Sign Up Today and get 10% OFF! Use code WELCOME to redeem!
-        <StyledCloseButton onClick={CloseButton}>X</StyledCloseButton>
-      </Welcome>
-
-      <StyledDiv>
-        <StyledLogo src={Logo} onClick={() => { navigate('/') }} />
-        <Button onClick={() => { navigate('/FindATutor') }} text='Find a Tutor' />
-        {LoggedIn != true ? <Button onClick={() => setButtonPopup(true)} text='Login'/> : <Button onClick={() => setButtonPopup(true)} text='Register' />}
-        <StyledImage src={Picture} onClick={() => { navigate('/Profile') }} text='Profile' />
-      </StyledDiv>
-    </>
-  );
 };
 
 export default NavigationBar
@@ -108,11 +83,11 @@ const StyledDiv = styled.div`
 display: flex;
 padding-left: 0px;
 padding-right: 20px;
-padding-top: 20px;
+padding-top: 120px;
 padding-bottom: 20px;
 gap: 20px;
 justify-content: space-evenly;
-background-color: skyblue;
+background-color: #0d67b5;
 align-items: center;
 @media (max-width: 750px) {
   display: none;
@@ -120,22 +95,11 @@ align-items: center;
 `
 
 const StyledLogo = styled.img`
-height: 120px;
-`
-
-
-const Welcome = styled.div`
-width: -webkit-fill-available;
-text-align: center;
-background-color: blue;
-color: white;
-padding: 10px;
-// margin-top: -60px;
-margin-top: 0px;
-transition: 0.69s ease;
-&::selection {
-  background: black;
-}
+position: absolute;
+height: 140px;
+width: 450px;
+top: 0;
+object-fit: cover;
 `
 
 const StyledCloseButton = styled.button`
@@ -147,23 +111,16 @@ position: absolute;
 right: 10px;
 `
 
-const StyledImage = styled.img`
-height: 120px;
-width: 120px;
-border-radius: 50%; // This makes the image circular
-object-fit: cover // This prevents the image from stretching
-`
-
 const StyledSideBar = styled.div`
 display: flex;
 justify-content: flex-start;
 flex-wrap: wrap;
 flex-direction: column;
-background-color: skyblue;
+background-color: cornflowerblue;
 position: fixed;
 top: 0;
 bottom: 0;
-left: 0;
+left: -100%;
 padding-top: 40px;
 transition: 1s cubic-bezier(0.2, 0.8, 0.2, 1);
 `
@@ -201,7 +158,7 @@ background-color: transparent;
 const CookieConsent = styled.div`
 width: -webkit-fill-available;
 text-align: center;
-background-color: blue;
+background-color: orangered;
 color: white;
 padding: 10px;
 bottom: 0;
@@ -209,5 +166,15 @@ position: fixed;
 transition: 0.69s ease;
 &::selection {
   background: black;
+}
+`
+
+const CookieConsentButton = styled.button`
+background-color: transparent;
+border-color: transparent;
+color: white;
+font-weight: bold;
+&:hover {
+  background-color: black;
 }
 `
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,73 @@
 import { styled } from "styled-components";
 import NavigationBar from "../Navigation/NavigationBar";
 import React from "react";
-import ChatBot from "../components/ChatBot/ChatBox";
+import VerticlePersonHome from "../assets/StockPersonVerticleHome.jpg";
+import StockTeacherVerticleHome from "../assets/StockTeacherVerticleHome.jpg";
 
 export default function Home() {
   return (
     <>
-      <ChatBot />
-      <NavigationBar />
-      <StyledDiv>Home</StyledDiv>
+      <StyledTopDiv>
+
+        <NavigationBar />
+        <StyledHomeTopText>Welcome to GibJohn Tutoring!</StyledHomeTopText>
+        <StyledImageVerticlePersonHome src={VerticlePersonHome}/>
+      </StyledTopDiv>
+        <StyledHomeMainText>
+        Are you looking for personalized and effective tutoring to help you excel in your studies? Look no further! Our team of experienced tutors is here to support you every step of the way.
+        <br/>
+        <br/>
+        Whether you need help with math, science, language arts, or any other subject, we've got you covered. Our tutors are highly qualified and passionate about helping students reach their full potential.
+        <br/>
+        <br/>
+        Sign up now to unlock a world of knowledge and academic success. Gain access to one-on-one tutoring sessions, tailored study materials, and a supportive learning community. Don't miss out on this opportunity to boost your grades and build confidence in your abilities.
+        <br/>
+        <br/>
+        Join us today and embark on a journey towards academic excellence. Sign up now and take the first step towards achieving your goals!
+        <StyledImageVerticleTeacherHome src={StockTeacherVerticleHome}/>
+        </StyledHomeMainText>
     </>
   );
 }
 
-const StyledDiv = styled.div`
+const StyledTopDiv = styled.div`
   font-size: 40px;
-  padding-top: 20px;
+  background-color: cornflowerblue;
+  padding-bottom: 600px;
+  color: white;
+  `;
+
+const StyledImageVerticlePersonHome = styled.img`
+height: 30vh;
+position: absolute;
+top: 25%;
+left: 60%;
+transform: scaleX(-1);
+`;
+
+
+const StyledHomeTopText = styled.h1`
+  font-size: 40px;
+  top: 35%;
+  left: 42%;
+  position: absolute;
+  transform: translate(-50%, -50%);
+  `;
+
+const StyledHomeMainText = styled.p`
+font-size: 24px;
+text-align: center;
+padding-left: 600px;
+padding-right: 100px;
+padding-top: 100px;
+color: #0a3d48;
+font-family: system-ui;
+  `;
+
+const StyledImageVerticleTeacherHome = styled.img`
+height: 80vh;
+position: absolute;
+top: 77%;
+left: 0%;
+z-index: -1;
 `;
